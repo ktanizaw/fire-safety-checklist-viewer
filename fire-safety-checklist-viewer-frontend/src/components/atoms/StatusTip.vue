2016df3ad17a26ff257b1495004468e8d34d962f
ktanizaw <kt10025102@gmail.com>
2025-08-13 19:44:22 +0100

    feat: add status tip component

---
+++ fire-safety-checklist-viewer-frontend/src/components/atoms/StatusTip.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+defineProps<{
+  color: string;
+  text: string;
+}>();
+</script>
+
+<template>
+  <span class="status-tip" :class="`status-tip--${color}`">
+    {{ text }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+.status-tip {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  height: 18px;
+  padding: 2px 4px;
+  border-radius: 4px;
+  font-size: $text-xxs;
+
+  &--blue {
+    background-color: $color-blue-light;
+    color: $color-blue;
+  }
+
+  &--green {
+    background-color: $color-green-light;
+    color: $color-green;
+  }
+
+  &--yellow {
+    background-color: $color-yellow-light;
+    color: $color-yellow;
+  }
+
+  &--red {
+    background-color: $color-red-light;
+    color: $color-red;
+  }
+}
+</style>
