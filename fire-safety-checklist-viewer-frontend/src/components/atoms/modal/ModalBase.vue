1f6723b18708b9cdc77dd46a63eb67a809b9caa1
ktanizaw <kt10025102@gmail.com>
2025-08-14 17:43:39 +0100

    feat: add modal base component

---
+++ fire-safety-checklist-viewer-frontend/src/components/atoms/modal/ModalBase.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="modal">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  width: 100%;
+  min-width: inherit;
+  height: 100%;
+  overflow-y: scroll;
+  background: rgba($color-black, 0.5);
+  inset: 0;
+}
+</style>
