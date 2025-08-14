6313ff07bf9364e8e177e6466cca918b41349e92
ktanizaw <kt10025102@gmail.com>
2025-08-14 23:55:56 +0100

    feat:add wrapper function for useAsyncData

---
+++ fire-safety-checklist-viewer-frontend/src/composables/useRequiredAsyncData.ts
@@ -0,0 +1,32 @@
+import type { AsyncDataOptions, NuxtApp } from "nuxt/app";
+
+export const useRequiredAsyncData = async <T>(
+  handler: (ctx?: NuxtApp) => Promise<T>,
+  options?: AsyncDataOptions<T>
+) => {
+  const { data, error, ...res } = await useAsyncData<T>(handler, options);
+
+  if (error.value) {
+    const statusCode = isNuxtError(error.value) ? error.value.statusCode : 500;
+    const message = error.value.message || "server error";
+
+    throw createError({
+      statusCode,
+      message,
+      fatal: true,
+    });
+  }
+
+  const readData = computed(() => {
+    if (!data.value) {
+      throw new Error("data is not fetched");
+    }
+    return data.value;
+  });
+
+  return {
+    data: readData,
+    error,
+    ...res,
+  };
+};
