848b9efd662a2100864cc581f256d972f9e3b31a
ktanizaw <kt10025102@gmail.com>
2025-08-15 11:33:59 +0100

    feat:add transition component

---
+++ fire-safety-checklist-viewer-frontend/src/components/transition/FadeTransition.vue
@@ -0,0 +1,17 @@
+<template>
+  <Transition name="fade" mode="out-in">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
